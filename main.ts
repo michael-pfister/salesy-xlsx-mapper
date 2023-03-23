14c7303fa1297f8b4506518c16da2cacfd28ddd5
michael-pfister <schoko.bombe44@gmail.com>
2023-03-23 14:47:05 +0100

    add vscode config

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 // @deno-types="https://cdn.sheetjs.com/xlsx-0.19.2/package/types/index.d.ts"
 import * as XLSX from 'https://cdn.sheetjs.com/xlsx-0.19.2/package/xlsx.mjs';
-console.log(XLSX);
+
+const subsectors = XLSX.readFile('linkedin_subsectors.csv');
+const industries = XLSX.readFile('linkedin_industries.csv');
