51970a9b34610b633890b879362782201f452df3
michael-pfister <schoko.bombe44@gmail.com>
2023-03-27 10:49:43 +0200

    filter out 'and' and ','

--- main.ts
+++ main.ts
@@ -7,44 +7,58 @@ const industries = XLSX.readFile("linkedin_industries.csv").Sheets["Sheet1"];
 let count = 0;
 
 Object.keys(subsectors).map((cell: string) => {
-  const { subsectorId, subsectorType, subsectorValue } = {
-    subsectorId: Number(cell.slice(1)),
-    subsectorType: subsectors[cell].t,
-    subsectorValue: subsectors[cell].v,
-  };
+	const { subsectorId, subsectorType, subsectorValue } = {
+		subsectorId: Number(cell.slice(1)),
+		subsectorType: subsectors[cell].t,
+		subsectorValue: subsectors[cell].v,
+	};
 
-  if (subsectorType === "s" && subsectorValue) {
-    Object.keys(industries).map((cell: string) => {
-      const { industryId, industryType, industryValue } = {
-        industryId: Number(cell.slice(1)),
-        industryType: industries[cell].t,
-        industryValue: industries[cell].v,
-      };
+	if (subsectorType === "s" && subsectorValue) {
+		Object.keys(industries).map((cell: string) => {
+			const { industryId, industryType, industryValue } = {
+				industryId: Number(cell.slice(1)),
+				industryType: industries[cell].t,
+				industryValue: industries[cell].v,
+			};
 
-      if (industryType === "s" && industryValue) {
-        if (
-          industryValue.toLowerCase().includes(subsectorValue.toLowerCase())
-        ) {
-          XLSX.utils.sheet_add_aoa(industries, [[subsectorId]], {
-            origin: `B${industryId}`,
-          });
+			if (industryType === "s" && industryValue) {
+				if (
+					industryValue.toLowerCase().includes(subsectorValue.toLowerCase())
+				) {
+					XLSX.utils.sheet_add_aoa(industries, [[subsectorId]], {
+						origin: `B${industryId}`,
+					});
 
-          count++;
-        }else if (industryValue.toLowerCase().split(' ').some((r: string) => subsectorValue.toLowerCase().split(' ').indexOf(r) >= 0)){
-          XLSX.utils.sheet_add_aoa(industries, [[subsectorId]], {
-            origin: `B${industryId}`,
-          });
+					count++;
+				} else if (
+					industryValue
+						.toLowerCase()
+						.split(" ")
+						.filter((word: string) => word !== "and")
+						.map((word: string) => word.replaceAll(",", ""))
+						.some(
+							(r: string) =>
+								subsectorValue
+									.toLowerCase()
+									.split(" ")
+									.map((word: string) => word.replaceAll(",", ""))
+									.indexOf(r) >= 0
+						)
+				) {
+					XLSX.utils.sheet_add_aoa(industries, [[subsectorId]], {
+						origin: `B${industryId}`,
+					});
 
-          count++;
-        }
-      }
-    });
-  }
+					count++;
+				}
+			}
+		});
+	}
 });
 
 console.log(`${count} subsectors added to industries`);
 
 XLSX.writeFile(
-  { Sheets: { Sheet1: industries }, SheetNames: ["Sheet1"] },
-  "linkedin_industries_new.xlsx"
+	{ Sheets: { Sheet1: industries }, SheetNames: ["Sheet1"] },
+	"linkedin_industries_new.xlsx"
 );
